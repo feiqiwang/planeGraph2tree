0b9f21b64ec72c95b3821c991008e6d82b99e399
feiqiwang <55272046+feiqiwang@users.noreply.github.com>
2020-07-14 17:51:22 +0900

    Add files via upload

--- ComparisonProgram/clusteringComputation.r
+++ ComparisonProgram/clusteringComputation.r
@@ -2,4 +2,4 @@
 
 TreeEditDistanceResult <- as.dist(read.csv(file = "treeEditDistanceResult.CSV",header = TRUE, row.name = 1))
 clusterResult <- hclust(TreeEditDistanceResult, "ward.D2")
-plot(clusterResult,hang = -1,main = "Clustering Result for transferred RNA secondary structure")
+plot(clusterResult,hang = -1,main = "",sub="",xlab="",ylab="",axes=FALSE)
